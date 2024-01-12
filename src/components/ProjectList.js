ba0940a1189a87bc013bdd550ba1dc974e5b81eb
Alanquintana0 <65621524+Alanquintana0@users.noreply.github.com>
2024-01-11 21:32:44 -0700

    Fixed image routes

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -10,7 +10,7 @@ export const ProjectList = ({limite}) => {
           return(
             <article key={project.id} className='work-item'>
               <div className='mask'>
-                <img src={'/images/'+project.id+".png"}/>
+                <img src={'/portafolio/images/'+project.id+".png"}/>
               </div>
               <span>{project.categorias}</span>
               <h2><Link to={"/project/"+project.id}>{project.nombre}</Link></h2>
