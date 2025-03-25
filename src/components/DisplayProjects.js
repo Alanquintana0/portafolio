23d37e834f3868c545106a6bc4c642edf0a9d8c0
Alanquintana0 <65621524+Alanquintana0@users.noreply.github.com>
2025-03-24 22:47:55 -0600

    Ahora se obtienen los proyectos desde un backend local

--- src/components/DisplayProjects.js
+++ src/components/DisplayProjects.js
@@ -1,10 +1,35 @@
 import { ProjectCard } from './ProjectCard';
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { projects } from '../data/projects';
+// import { projects } from '../data/projects';
 
 export const DisplayProjects = () => {
-    
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        fetchProjects();
+    }, []);
+
+    const fetchProjects = async () => {
+        try{
+            const res = await fetch('http://localhost:8080/api/v1/projects');
+            if(!res.ok){
+                throw new Error('response not ok');
+            }
+            const data = await res.json();
+            console.log(data);
+            setProjects(data);
+            setLoading(false);
+        }catch(er){
+            setError(er.message);
+            setLoading(false);
+        }
+    }
+
+    if(loading) return <div>loading projects..</div>;
+    if(error) return <div>Error: {Error}</div>;
 
 
     return (
@@ -16,7 +41,7 @@ export const DisplayProjects = () => {
                 {
                     projects.map(project => {
                         return(
-                            <ProjectCard key={project.id} project={project}></ProjectCard>
+                            <ProjectCard key={project.projectId} project={project}></ProjectCard>
                         )
                     })
                 }
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
 
 
 export const ProjectCard = ({ project }) => {
-  const params = useParams();
 
   return (
     <div className='card-container'>
         <div className="card-img">
-        <img src={`/images/${project.id}.png`} alt='Imagen de proyecto'/>
+        <img src={`/images/${project.idName}.png`} alt='Imagen de proyecto'/>
         </div>
         <div className="card-description">
-            <h1>{project.nombre}</h1>
-            <p>{ project.descripcion }</p>
-            <p>{ project.categorias }</p>
+            <h1>{project.name}</h1>
+            <p>{ project.description }</p>
+            <p>{ project.category }</p>
         </div>
     </div>
   )
